28e306de03704fa74a4fc97b3840a5b3cdcdfad9
chapal <siarhei.sharykhin@gmail.com>
2015-05-23 23:24:53 +0300

    complete pipelines

--- model_concurrency/sq.go
+++ model_concurrency/sq.go
@@ -5,28 +5,38 @@ import (
 	"sync"
 )
 
-func gen(nums ...int) <-chan int {
-	out := make(chan int)
+func gen(done <-chan struct{}, nums ...int) <-chan int {
+	out := make(chan int, len(nums))
 
 	go func() {
-		for _, n := range nums {
-			out <- n
-		}
 		defer close(out)
+
+		for _, n := range nums {
+			select {
+			case out <- n:
+			case <-done:
+				return
+			}
+		}
+
 	}()
 
 	return out
 }
 
-func sq(in <-chan int) <-chan int {
+func sq(done <-chan struct{}, in <-chan int) <-chan int {
 	out := make(chan int)
 
 	go func() {
+		defer close(out)
 
 		for n := range in {
-			out <- n * n
+			select {
+			case out <- n * n:
+			case <-done:
+				return
+			}
 		}
-		defer close(out)
 
 	}()
 
@@ -34,24 +44,31 @@ func sq(in <-chan int) <-chan int {
 }
 
 func main() {
-	in := gen(2, 3, 4, 5, 6, 7, 8, 9)
+	done := make(chan struct{})
+	defer close(done)
+	in := gen(done, 2, 3)
 
-	c1 := sq(in)
-	c2 := sq(in)
-	c3 := sq(in)
+	c1 := sq(done, in)
+	c2 := sq(done, in)
 
-	for n := range merge(c1, c2, c3) {
-		fmt.Println(n)
-	}
+	out := merge(done, c1, c2)
+
+	fmt.Println(<-out)
+
 }
 
-func merge(cs ...<-chan int) <-chan int {
+func merge(done <-chan struct{}, cs ...<-chan int) <-chan int {
 	var wg sync.WaitGroup
 	out := make(chan int)
 
 	output := func(c <-chan int) {
+		defer wg.Done()
 		for n := range c {
-			out <- n
+			select {
+			case out <- n:
+			case <-done:
+				return
+			}
 		}
 		wg.Done()
 	}
