a9b074440d3b7cd51cfe862e8414dc0e7c374c94
Sharykhin <siarhei.sharykhin@gmail.com>
2015-06-17 08:41:58 +0300

    Add copying of slices

---
+++ traps/test9.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	s1 := []int{1, 2, 3}
+	fmt.Println(len(s1), cap(s1), s1)
+
+	s2 := make([]int, 2)
+	copy(s2, s1[1:])
+	fmt.Println(len(s2), cap(s2), s2)
+
+	for i := range s2 {
+		s2[i] += 20
+	}
+
+	fmt.Println(s1)
+	fmt.Println(s2)
+}
