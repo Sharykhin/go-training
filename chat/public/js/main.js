56f4565dd6900f3bea15297bc503edf9528836cc
Sharykhin <siarhei.sharykhin@gmail.com>
2015-07-08 09:39:02 +0300

    add userslist to server

--- chat/public/js/main.js
+++ chat/public/js/main.js
@@ -1,1 +1,10 @@
 var socket = io();
+$('.enter-name').on('click', function(event){
+	event.preventDefault();
+	$('.username').slideDown();
+});
+
+$('.submit-name').on('click', function(){
+	socket.emit('enter name', $('.username input[type=text]').val());
+	$('.username input[type=text]').val('');
+});
--- chat/server.go
+++ chat/server.go
@@ -11,15 +11,21 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	users := make(map[string]string)
 	server.On("connection", func(so socketio.Socket) {
-		log.Println("on connection")
+		users[so.Id()] = ""
+		log.Println("on connection", so.Id())
+		log.Println(users)
 		so.Join("chat")
-		so.On("chat message", func(msg string) {
-			log.Println("emit:", so.Emit("chat message", msg))
-			so.BroadcastTo("chat", "chat message", msg)
+		so.On("enter name", func(msg string) {
+			log.Println("emit:", so.Emit("enter name", msg))
+			users[so.Id()] = msg
+			//so.BroadcastTo("chat", "chat message", msg)
+			log.Println(users)
 		})
 		so.On("disconnection", func() {
 			log.Println("on disconnect")
+			delete(users, so.Id())
 		})
 	})
 	server.On("error", func(so socketio.Socket, err error) {
